067abe4043de033383fac2d7863cb75ca2e551c5
mher96 <mher.mshecyan@gmail.com>
2020-12-22 20:15:53 +0400

    load-sheet endpoint

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const { retrieveUsers } = require('../controllers/retrieveFromSheet')
 // const { saveUsers } = require('../controllers/saveToSheet')
 
-router.get('/v1/users', retrieveUsers)
+router.get('/v1/load-sheet', retrieveUsers)
+// router.get('/v1/load-data', )
 // router.post('/v1/update/users', saveUsers)
 
 module.exports = router
